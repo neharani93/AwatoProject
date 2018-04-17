9ab4192113b3fc702fe6d844861ab2e6037b86e2
NGOENKA <NGOENKA>
2018-04-17 16:46:39 -0400

    Working with Actual Data-Value Lost By Airline per Month

--- scripts/Controllers/ValueLostByAirline.js
+++ scripts/Controllers/ValueLostByAirline.js
@@ -1,14 +1,14 @@
-app.controller('valueLostByAirlineCtrl', function ($scope) {
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Delta Airlines', 'American Airlines' ,'USAir'];
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
+app.controller('valueLostByAirlineCtrl', function ($scope) {    
+    $scope.labels = [];
+    $scope.series = [];
+    $scope.data = [];
+
     $scope.onClick = function (points, evt) {
         console.log(points, evt);
     };
-    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+
+    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
+
     $scope.options = {
         scales: {
             yAxes: [
@@ -17,14 +17,56 @@ app.controller('valueLostByAirlineCtrl', function ($scope) {
                   type: 'linear',
                   display: true,
                   position: 'left'
-              },
-              {
-                  id: 'y-axis-2',
-                  type: 'linear',
-                  display: true,
-                  position: 'right'
-              }
+              }              
             ]
         }
     };
+
+    activate();
+
+    function activate()
+    {       
+        for (var i = 0; i < claimsData.length; i++)
+        {
+            //Get Airlines Names - series
+            if ($scope.series.indexOf(claimsData[i].airlineName) == -1) {
+                $scope.series.push(claimsData[i].airlineName);
+            }
+
+            //Get Months-labels for X-axis
+            var claimDate = claimsData[i].dateReceived;
+            var month = claimDate.slice(claimDate.indexOf('-') + 1, claimDate.length);
+            if ($scope.labels.indexOf(month) == -1) {
+                $scope.labels.push(month);
+            }
+        }
+
+        //Calculate Value Lost Per Month by Airline
+        var amtLostByAirlineByMonth = [];
+        for (var i = 0; i < $scope.series.length; i++)
+        {
+			//Initializing an Array for Value Lost per Airline to Zero acrooss All Data Points.
+            var currAirlineData = Array.apply(null, Array($scope.labels.length)).map(Number.prototype.valueOf, 0);
+
+            for (var j = 0; j < $scope.labels.length; j++)
+            {               
+                for (var k = 0; k < claimsData.length; k++)
+                {
+                    var claimDate = claimsData[k].dateReceived;
+                    var month = claimDate.slice(claimDate.indexOf('-') + 1, claimDate.length);
+
+                    if ((claimsData[k].airlineName == $scope.series[i]) && (claimsData[k].disposition == "Approve in Full"))
+                    {
+                        if (month == $scope.labels[j])
+                        {
+                            currAirlineData[j] = currAirlineData[j] + parseFloat(claimsData[k].closeAmount.replace("$", '').replace(",", ''));
+                        }
+                    }
+                }
+            }
+            amtLostByAirlineByMonth.push(currAirlineData);
+        }
+
+        $scope.data = amtLostByAirlineByMonth;
+    }
 });
