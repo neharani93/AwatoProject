d4ac7e8a1f0bf6361424c6acf61806e2c7de3425
NGOENKA <NGOENKA>
2018-04-18 10:27:38 -0400

    Claims by Aiport with filters

--- scripts/Controllers/ClaimsByAirport.js
+++ scripts/Controllers/ClaimsByAirport.js
@@ -1,9 +1,72 @@
 app.controller('claimsByAirportCtrl', function ($scope) {
-    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    $scope.series = ['Series A', 'Series B'];
+    //Hardcoding these, there are ton of them. Not parsing data to obtain these, coz these might be coming form differnt table in the db and 
+	//we do not have to parse such a big dataset always to obtain these.
+	$scope.airports = {EWR: true, JFK: true, SFO: true, HOU: true};	
+	$scope.years = {2010: true, 2011:false, 2012:true, 2013 : false};
+	
+	$scope.onFiltersChange = function()
+	{
+		renderChart();
+	}   
+	
+	$scope.options = {      
+        legend: {
+            display: true           
+        }
+    };
 
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 54],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
+   renderChart();
+
+    function renderChart()
+    {
+		$scope.labels = [];
+		$scope.data = [];
+		$scope.series = [];
+		
+		//Parse the airport selections to get the series
+		var airportKeyNames = Object.keys($scope.airports);
+		for(var i=0; i<airportKeyNames.length; i++){
+			if($scope.airports[airportKeyNames[i]]){
+				$scope.series.push(airportKeyNames[i]);
+			}
+		}		
+			
+		//Parse the data to get the data points	for x-axis		
+        for (var i = 0; i < claimsData.length; i++)
+        {
+            //Get Months-labels for X-axis
+            var claimDate = claimsData[i].dateReceived;
+            var month = claimDate.slice(claimDate.indexOf('-') + 1, claimDate.length);
+			
+			var jsDate = new Date(claimDate);			
+			var claimYear = jsDate.getFullYear();
+			
+            if ($scope.labels.indexOf(month) == -1 && $scope.years[claimYear]) {
+                $scope.labels.push(month);
+            }
+        }
+        
+		//Calculate of claims per airport across all the data-points
+        var avgClaimsByAirport = [];
+        for (var i = 0; i < $scope.series.length; i++) {		
+            var currAirportVolume = Array.apply(null, Array($scope.labels.length)).map(Number.prototype.valueOf, 0);
+
+            for (var j = 0; j < $scope.labels.length; j++) {
+                for (var k = 0; k < claimsData.length; k++) {
+                    var claimDate = claimsData[k].dateReceived;
+                    var month = claimDate.slice(claimDate.indexOf('-') + 1, claimDate.length);					
+				
+					if (claimsData[k].airportCode == $scope.series[i]) {
+						if (month == $scope.labels[j]) {
+							currAirportVolume[j] = currAirportVolume[j] + 1;							
+						}
+					}										
+                }
+            }
+            avgClaimsByAirport.push(currAirportVolume);
+        }
+
+        $scope.data = avgClaimsByAirport;
+    }
+
 });
